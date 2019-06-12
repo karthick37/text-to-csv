c45b1b24dd24f23ef00565223d349a08988df682
karthick37 <40197179+karthick37@users.noreply.github.com>
2019-06-12 17:48:41 +0530

    Add files via upload

--- tsvtocsv.py
+++ tsvtocsv.py
@@ -1,7 +1,11 @@
 import re
-with open("/home/arun/Documents/Shohaney.factera.fusions.txt", 'r') as myfile:
-     with open("/home/arun/Documents/Shohaney.factera.fusions.csv", 'w') as csv_file:
+import sys
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+with open(inputfile, 'r') as myfile:
+     with open(outputfile, 'w') as csv_file:
              for line in myfile:
                      fileContent = re.sub("\t", ",", line)
                      csv_file.write(fileContent)
-
